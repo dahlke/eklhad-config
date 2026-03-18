22bb57033239d9710977857f9dbbcd9802ffc15e
Neil Dahlke <neil.dahlke@gmail.com>
2026-03-18 12:41:15 -0700

    fmt

--- terraform/rasppi/variables.tf
+++ terraform/rasppi/variables.tf
@@ -1,19 +1,19 @@
 variable "rpi_addresses" {
-	type = map
+  type = map
 }
 
 variable "rpi_leader_address" {
-	type = string
+  type = string
 }
 
 variable "rpi_follower_addresses" {
-	type = map
+  type = map
 }
 
 variable "rpi_username" {
-	type = string
+  type = string
 }
 
 variable "rpi_ssh_priv_key_path" {
-	type = string
+  type = string
 }
